0762e9f842ec83c858c272fe44e2fcbf2cdbd740
Dmitrii Golub <dmitrii.golub@gmail.com>
2012-07-11 12:54:18 +0400

    update group fields

--- lib/vk/api_params.rb
+++ lib/vk/api_params.rb
@@ -1,8 +1,14 @@
 module Vk
   module ApiParams
     AVAILABLE_FIELDS = %w(uid first_name last_name nickname screen_name sex bdate city country timezone photo photo_medium photo_big has_mobile rate contacts education online counters)
+    GROUP_FIELDS = %w(city country place description wiki_page)
+
     def all_fields
-      AVAILABLE_FIELDS.join(',')
+      AVAILABLE_FIELDS
+    end
+
+    def all_group_fields
+      GROUP_FIELDS
     end
   end
 end
