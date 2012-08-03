7969a9708caa02b9c76e3ca4eef033cf29cbdd29
Claster <dr.korbin@gmail.com>
2012-08-03 15:49:45 +0400

    More fields

--- lib/vk/api_params.rb
+++ lib/vk/api_params.rb
@@ -1,7 +1,7 @@
 module Vk
   module ApiParams
-    AVAILABLE_FIELDS = %w(uid first_name last_name nickname screen_name sex bdate city country timezone photo photo_medium photo_big has_mobile rate contacts education online counters)
-    GROUP_FIELDS = %w(city country place description wiki_page)
+    AVAILABLE_FIELDS = %w(uid first_name last_name sex bdate city country photo photo_medium photo_medium_rec photo_big photo_rec online lists screen_name has_mobile rate contacts education universities schools can_post can_see_all_posts can_write_private_message activity last_seen relation counters nickname wall_comments interests movies tv books games about connections relatives timezone)
+    GROUP_FIELDS = %w(city country place description wiki_page members_count counters start_date end_date)
 
     def all_fields
       AVAILABLE_FIELDS
